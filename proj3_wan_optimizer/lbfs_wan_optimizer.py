2a367840532e9da12bd2f8579ec0887c32044385
Casey <kazehero@github.com>
2017-11-07 19:34:33 -0800

    one test case left

--- proj3_wan_optimizer/lbfs_wan_optimizer.py
+++ proj3_wan_optimizer/lbfs_wan_optimizer.py
@@ -32,12 +32,13 @@ class WanOptimizer(wan_optimizer.BaseWanOptimizer):
         the WAN; this WAN optimizer should operate based only on its own local state
         and packets that have been received.
         """
-        packet_size = packet.size()
+
         if packet.dest not in self.hanging_destinations:
             # initialize hanging_msgs for destinations
             self.hanging_destinations[packet.dest] = ""
 
-        if packet_size > utils.MAX_PACKET_SIZE:
+        packet_size = self.hanging_destinations[packet.dest].__len__() + packet.size()
+        if packet_size > self.hanging_destinations[packet.dest].__len__() + utils.MAX_PACKET_SIZE:
             # For all communication, the packet size should be less than or equal to utils.MAX_PACKET_SIZE
             return
 
@@ -65,34 +66,63 @@ class WanOptimizer(wan_optimizer.BaseWanOptimizer):
             # The packet must be destined to a host connected to the other middle box
             # so send it across the WAN. Bytes sent from client to WAN.
             if packet.is_fin and packet_size == 0:
+                # final packet with no data
                 self.handle_drop(packet)
                 return
+
+            # Set up scanning through the packet
             start_block = 0
             start_window = 0
             end_window = self.window_size
+            # Include hanging message from before
             copied_payload = self.hanging_destinations[packet.dest] + packet.payload[:]
             while end_window <= packet_size:
                 window_msg = copied_payload[start_window:end_window]
                 hashed_msg = utils.get_hash(window_msg)
                 if self.GLOBAL_MATCH_BITSTRING == utils.get_last_n_bits(hashed_msg, 13):
-                    print("wtf")
-                    # Proceed with messages before window_msg
+                    # No more hanging message!
                     self.hanging_destinations[packet.dest] = ""
+                    split_start = 0
+                    split_end = utils.MAX_PACKET_SIZE
                     block_msg = copied_payload[start_block:end_window]
-                    block_hashed_msg = utils.get_hash(block_msg)
-                    if block_hashed_msg not in self.buffered_hashes:
-                        self.buffered_hashes.add(block_hashed_msg)
-                        packet.payload = block_msg
-                        packet.is_raw_data = True
-                    else:
-                        packet.payload = block_hashed_msg
-                        packet.is_raw_data = False
-                    self.send(packet, self.wan_port)
+                    block_msg_size = block_msg.__len__()
+
+                    # Split big block into block sized packets if necessary
+                    while split_end < block_msg_size:
+                        split_block_msg = block_msg[split_start:split_end]
+                        split_block_hashed_msg = utils.get_hash(split_block_msg)
+                        if split_block_hashed_msg not in self.buffered_hashes:
+                            self.buffered_hashes.add(split_block_hashed_msg)
+                            packet.is_raw_data = True
+                            packet.payload = split_block_msg
+                        else:
+                            packet.is_raw_data = False
+                            packet.payload = split_block_hashed_msg
+                        self.send(packet, self.wan_port)
+                        split_start = split_end
+                        split_end = split_end + utils.MAX_PACKET_SIZE
+
+                    # Leftover msg?
+                    leftover_block_msg = block_msg[split_start:]
+                    if leftover_block_msg != "":
+                        leftover_hashed_msg = utils.get_hash(leftover_block_msg)
+                        if leftover_hashed_msg not in self.buffered_hashes:
+                            self.buffered_hashes.add(leftover_hashed_msg)
+                            packet.is_raw_data = True
+                            packet.payload = leftover_block_msg
+                        else:
+                            packet.is_raw_data = False
+                            packet.payload = leftover_hashed_msg
+                        self.send(packet, self.wan_port)
+
+                    # Restart block / window size
                     start_block, start_window = end_window, end_window
                     end_window += self.window_size
                 else:
                     start_window += 1
                     end_window += 1
+
+            # Partake in storing hanging messages...
             start_block = self.hanging_destinations[packet.dest].__len__() if start_block == 0 else start_block
             self.hanging_destinations[packet.dest] += copied_payload[start_block:]
             if packet.is_fin:
@@ -119,15 +149,16 @@ class WanOptimizer(wan_optimizer.BaseWanOptimizer):
                 self.send(packet, self.wan_port)
                 start_block = end_block
                 end_block = end_block + utils.MAX_PACKET_SIZE
-            leftover_block_msg = hanging_msg[start_block:end_block]
-            hashed_msg = utils.get_hash(leftover_block_msg)
             packet.is_fin = True
-            if hashed_msg not in self.buffered_hashes:
-                self.buffered_hashes.add(hashed_msg)
-                packet.payload = leftover_block_msg
-                packet.is_raw_data = True
-            else:
-                packet.payload = hashed_msg
-                packet.is_raw_data = False
+            leftover_block_msg = hanging_msg[start_block:]
+            if leftover_block_msg != "":
+                hashed_msg = utils.get_hash(leftover_block_msg)
+                if hashed_msg not in self.buffered_hashes:
+                    self.buffered_hashes.add(hashed_msg)
+                    packet.payload = leftover_block_msg
+                    packet.is_raw_data = True
+                else:
+                    packet.payload = hashed_msg
+                    packet.is_raw_data = False
         self.hanging_destinations[packet.dest] = ""
         self.send(packet, self.wan_port)
